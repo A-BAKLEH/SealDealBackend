9854ee950815a6ec24c6825bb96999c7271b870b
Athanas <athanas2000@gmail.com>
2022-07-25 16:50:27 -0400

    BillingController with Specification for Broker

---
+++ src/Clean.Architecture.Core/BrokerAggregate/Specifications/BrokerByIdWithAgencySpec.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ardalis.Specification;
+
+namespace Clean.Architecture.Core.BrokerAggregate.Specifications;
+public class BrokerByIdWithAgencySpec : Specification<Broker>, ISingleResultSpecification
+{
+  public BrokerByIdWithAgencySpec(Guid brokerId)
+  {
+    Query.Where(broker => broker.Id == brokerId)
+      .Include(broker => broker.Agency);
+  }
+}
--- src/Clean.Architecture.Web/Api/BillingController/BillingController.cs
+++ src/Clean.Architecture.Web/Api/BillingController/BillingController.cs
@@ -1,10 +1,13 @@
 ﻿using Clean.Architecture.Core.AgencyAggregate;
+using Clean.Architecture.Core.BrokerAggregate;
+using Clean.Architecture.Core.BrokerAggregate.Specifications;
 using Clean.Architecture.SharedKernel.Interfaces;
 using Clean.Architecture.Web.ApiModels;
 using Clean.Architecture.Web.AuthenticationAuthorization;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 
 namespace Clean.Architecture.Web.Api.BillingController;
 [Route("api/[controller]")]
@@ -12,16 +15,57 @@ namespace Clean.Architecture.Web.Api.BillingController;
 public class BillingController : BaseApiController
 {
 
-  private readonly IRepository<Agency> _repository;
-  public BillingController(IRepository<Agency> repository, AuthorizeService authorizeService) : base(authorizeService)
+  private readonly IRepository<Broker> _repository;
+  public BillingController(IRepository<Broker> repository, AuthorizeService authorizeService) : base(authorizeService)
   {
     _repository = repository;
   }
+
 
   [Authorize]
   [HttpPost("customer-portal")]
   public async Task<IActionResult> CustomerPortal([FromBody] CustomerPortalRequestDTO req)
   {
+    StripeConfiguration.ApiKey = "sk_test_51LHCXSIAg7HKu3" +
+               "TPU6Ess0RMvvdMbFiZw0GwWfgDqZkFUFXtYwTY5XRbjqyJrAnJ8arSQ12k3heATZSbsK6GJyEI00txFG34FH";
+
+    Guid b2cBrokerId;
+
+    try
+    {
+      var isAuth = User.Identity.IsAuthenticated;
+      if (!isAuth)
+      {
+        throw new Exception("not auth");
+      }
+      var l = User.Claims.ToList();
+
+      b2cBrokerId = Guid.Parse(l.Find(x => x.Type == "http://schemas.microsoft.com/identity/claims/objectidentifier").Value);
+
+
+      //var authAgency = _agencyRepository.GetById(authAdmin.AgencyId);
+      var authAdmin = await _repository.GetBySpecAsync(new BrokerByIdWithAgencySpec(b2cBrokerId));
+      var authAgency = authAdmin.Agency;
+
+      var options = new Stripe.BillingPortal.SessionCreateOptions
+      {
+        Customer = authAgency.AdminStripeId,
+        ReturnUrl = req.ReturnUrl,
+      };
+      var service = new Stripe.BillingPortal.SessionService();
+      var session = await service.CreateAsync(options);
+
+      return Ok(new
+      {
+        url = session.Url
+      });
+    }
+    catch (StripeException e)
+    {
+      Console.WriteLine(e.StripeError.Message);
+      return BadRequest(e.StripeError.Message);
+    }
+
     return Ok();
   }
 }
