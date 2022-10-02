af6eea88ca986e479b443b0c3ab6525275ae12f0
Bashar Eskandar <basharo9999@hotmail.com>
2022-10-02 16:15:16 -0400

    fix3

--- src/Clean.Architecture.Infrastructure/ExternalServices/Stripe/StripeBillingPortalService.cs
+++ src/Clean.Architecture.Infrastructure/ExternalServices/Stripe/StripeBillingPortalService.cs
@@ -1,11 +1,20 @@
 ﻿
 
 using Clean.Architecture.Core.ExternalServiceInterfaces.StripeInterfaces;
+using Microsoft.Extensions.Configuration;
+using Stripe;
 using Stripe.BillingPortal;
 
 namespace Clean.Architecture.Infrastructure.ExternalServices.Stripe;
 public class StripeBillingPortalService : IStripeBillingPortalService
 {
+  private readonly IConfigurationSection _stripeConfigSection;
+  public StripeBillingPortalService(IConfiguration config)
+  {
+    _stripeConfigSection = config.GetSection("StripeOptions");
+
+    StripeConfiguration.ApiKey = _stripeConfigSection["APIKey"];
+  }
   public async Task<string> CreateStripeBillingSessionAsync(string AdminStripeId, string returnURL)
   {
     var options = new SessionCreateOptions
