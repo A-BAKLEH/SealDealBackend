67d338d1724f90dceba074e44962fba532d26ec1
BasharEskandar <basharo9999@hotmail.com>
2022-11-13 23:25:29 -0500

    walwalwalwalwal

--- src/Clean.Architecture.Web/ApiModels/RequestDTOs/CreateLeadDTO.cs
+++ src/Clean.Architecture.Web/ApiModels/RequestDTOs/CreateLeadDTO.cs
@@ -1,10 +1,17 @@
-﻿namespace Clean.Architecture.Web.ApiModels.RequestDTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Clean.Architecture.Web.ApiModels.RequestDTOs;
 
 public class CreateLeadDTO
 {
-  public string? LeadFirstName { get; set; }
-  public string? LeadLastName { get; set; }
+  [Required(AllowEmptyStrings = false)]
+  public string LeadFirstName { get; set; }
+
+  [Required(AllowEmptyStrings = false)]
+  public string LeadLastName { get; set; }
+  [Phone]
   public string? PhoneNumber { get; set; }
+  [EmailAddress]
   public string? Email { get; set; }
   public int? Budget { get; set; }
   public string? leadNote { get; set; }
