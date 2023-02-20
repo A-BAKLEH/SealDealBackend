13c911f2b0e750ead139d01563a4a2c1c996aeea
BasharEskandar <basharo9999@hotmail.com>
2023-02-19 21:02:29 -0500

    ldfgnlerkgn

---
+++ src/Infrastructure/Data/Config/TemplateConfig.cs
@@ -0,0 +1,12 @@
+﻿using Core.Domain.BrokerAggregate.Templates;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config;
+public class TemplateConfig : IEntityTypeConfiguration<Template>
+{
+  public void Configure(EntityTypeBuilder<Template> builder)
+  {
+    builder.Property(p => p.TimesUsed).IsConcurrencyToken();
+  }
+}
