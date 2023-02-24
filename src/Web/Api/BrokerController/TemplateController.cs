d171a5468b17fbb6c96d6955ff5586c4da5d236d
BasharEskandar <basharo9999@hotmail.com>
2023-02-24 18:56:09 -0500

    dsdas3

--- src/Web/Api/BrokerController/TemplateController.cs
+++ src/Web/Api/BrokerController/TemplateController.cs
@@ -92,4 +92,22 @@ public class TemplateController : BaseApiController
     return Ok(template);
   }
 
+  //e for email, s for sms
+  [HttpDelete("/{TemplateType}/{Id}")]
+  public async Task<IActionResult> DeleteBrokerTemplate(string TemplateType,int Id)
+  {
+    var id = Guid.Parse(User.Claims.ToList().Find(x => x.Type == "http://schemas.microsoft.com/identity/claims/objectidentifier").Value);
+    var brokerTuple = await this._authorizeService.AuthorizeUser(id);
+    if (!brokerTuple.Item2)
+    {
+      _logger.LogWarning("[{Tag}] inactive mofo User with UserId {UserId} tried to delete template", TagConstants.Inactive, id);
+      return Forbid();
+    }
+
+    var names = await _templatesQService.DeleteTemplateAsync(Id,TemplateType,id);
+    if(names.Any()) return BadRequest(names);
+    return Ok();
+  }
+
+
 }
--- src/Web/ControllerServices/QuickServices/TemplatesQService.cs
+++ src/Web/ControllerServices/QuickServices/TemplatesQService.cs
@@ -17,74 +17,95 @@ public class TemplatesQService
   {
     _appDbContext = appDbContext;
   }
-
-  public async Task<TemplateDTO> UpdateTemplateAsync(UpdateTemplateDTO dto, Guid brokerId)
+  public async Task<List<string>> DeleteTemplateAsync(int templateId, string tempType, Guid brokerId)
   {
-    dynamic template = await _appDbContext.Templates.FirstAsync(t =>t.Id == dto.TemplateId && t.BrokerId == brokerId);
-
-    if(dto.text != null) template.templateText= dto.text;
-    if(dto.TemplateName != null) template.Title = dto.TemplateName;
-    if(dto.TemplateType == "e" && dto.subject != null) template.EmailTemplateSubject= dto.subject;
-    template.Modified = DateTimeOffset.UtcNow;
-    await _appDbContext.SaveChangesAsync();
-    return template.MapToDTO();
-
-  }
-  public async Task<TemplateDTO> CreateTemplateAsync(CreateTemplateDTO dto, Guid brokerId)
-  {
-    if (_appDbContext.Templates.Any(t => t.BrokerId == brokerId && t.Title == dto.TemplateName))
-    {
-      throw new CustomBadRequestException($"template already exists with name '{dto.TemplateName}'", ProblemDetailsTitles.AlreadyExists);
-    }
-    if (string.IsNullOrWhiteSpace(dto.text) || string.IsNullOrWhiteSpace(dto.TemplateName)
-      || string.IsNullOrWhiteSpace(dto.TemplateName) || string.IsNullOrWhiteSpace(dto.TemplateType))
-    {
-      throw new CustomBadRequestException("empty input", ProblemDetailsTitles.EmptyInput);
-    }
-    Template template;
-    if (dto.TemplateType == "e")
-    {
-      if (string.IsNullOrWhiteSpace(dto.subject)) throw new CustomBadRequestException("empty input", ProblemDetailsTitles.EmptyInput);
-      template = new EmailTemplate
-      {
-        BrokerId = brokerId,
-        EmailTemplateSubject = dto.subject,
-        Modified = DateTime.UtcNow,
-        templateText = dto.text,
-        TimesUsed = 0,
-        Title = dto.TemplateName
-      };
-    }
-    else
-    {
-      template = new SmsTemplate
-      {
-        BrokerId = brokerId,
-        Modified = DateTime.UtcNow,
-        templateText = dto.text,
-        TimesUsed = 0,
-        Title = dto.TemplateName,
-      };
-    }
-    _appDbContext.Templates.Add(template);
-    await _appDbContext.SaveChangesAsync();
-    //return template;
-    return template.MapToDTO();
-  }
-
-  public async Task<AllTemplatesDTO> GetAllTemplatesAsync(Guid brokerId)
-  {
-
-    var templates = await _appDbContext.Templates
-      .OrderByDescending(t => t.Modified)
-      .Where(t => t.BrokerId == brokerId)
+    var actionPlans = await _appDbContext.ActionPlans.Where(ap => ap.BrokerId == brokerId)
+      .Select(ap => new { ap.Name, actions = ap.Actions.Where(a => a.DataId == templateId) })
       .ToListAsync();
-    AllTemplatesDTO allTemplatesDTO = new AllTemplatesDTO();
-    foreach (var template in templates)
+    var apNames = new List<string>();
+    foreach (var item in actionPlans)
     {
-      allTemplatesDTO.allTemplates.Add(template.MapToDTO());
+      if (item.actions.Any()) apNames.Add(item.Name);
+    }
+    if (!apNames.Any())
+    {
+      Template temp;
+      if (tempType == "e") temp = new EmailTemplate { Id = templateId };
+      else temp = new SmsTemplate { Id = templateId };
+
+      _appDbContext.Templates.Remove(temp);
+      await _appDbContext.SaveChangesAsync();
+    }
+    return apNames;
+  }
+
+
+    public async Task<TemplateDTO> UpdateTemplateAsync(UpdateTemplateDTO dto, Guid brokerId)
+    {
+      dynamic template = await _appDbContext.Templates.FirstAsync(t => t.Id == dto.TemplateId && t.BrokerId == brokerId);
+
+      if (dto.text != null) template.templateText = dto.text;
+      if (dto.TemplateName != null) template.Title = dto.TemplateName;
+      if (dto.TemplateType == "e" && dto.subject != null) template.EmailTemplateSubject = dto.subject;
+      template.Modified = DateTimeOffset.UtcNow;
+      await _appDbContext.SaveChangesAsync();
+      return template.MapToDTO();
+    }
+    public async Task<TemplateDTO> CreateTemplateAsync(CreateTemplateDTO dto, Guid brokerId)
+    {
+      if (_appDbContext.Templates.Any(t => t.BrokerId == brokerId && t.Title == dto.TemplateName))
+      {
+        throw new CustomBadRequestException($"template already exists with name '{dto.TemplateName}'", ProblemDetailsTitles.AlreadyExists);
+      }
+      if (string.IsNullOrWhiteSpace(dto.text) || string.IsNullOrWhiteSpace(dto.TemplateName)
+        || string.IsNullOrWhiteSpace(dto.TemplateName) || string.IsNullOrWhiteSpace(dto.TemplateType))
+      {
+        throw new CustomBadRequestException("empty input", ProblemDetailsTitles.EmptyInput);
+      }
+
+      Template template;
+      if (dto.TemplateType == "e")
+      {
+        if (string.IsNullOrWhiteSpace(dto.subject)) throw new CustomBadRequestException("empty input", ProblemDetailsTitles.EmptyInput);
+        template = new EmailTemplate
+        {
+          BrokerId = brokerId,
+          EmailTemplateSubject = dto.subject,
+          Modified = DateTime.UtcNow,
+          templateText = dto.text,
+          TimesUsed = 0,
+          Title = dto.TemplateName
+        };
+      }
+      else
+      {
+        template = new SmsTemplate
+        {
+          BrokerId = brokerId,
+          Modified = DateTime.UtcNow,
+          templateText = dto.text,
+          TimesUsed = 0,
+          Title = dto.TemplateName,
+        };
+      }
+      _appDbContext.Templates.Add(template);
+      await _appDbContext.SaveChangesAsync();
+      //return template;
+      return template.MapToDTO();
     }
 
-    return allTemplatesDTO;
+    public async Task<AllTemplatesDTO> GetAllTemplatesAsync(Guid brokerId)
+    {
+      var templates = await _appDbContext.Templates
+        .OrderByDescending(t => t.Modified)
+        .Where(t => t.BrokerId == brokerId)
+        .ToListAsync();
+      AllTemplatesDTO allTemplatesDTO = new AllTemplatesDTO();
+      foreach (var template in templates)
+      {
+        allTemplatesDTO.allTemplates.Add(template.MapToDTO());
+      }
+
+      return allTemplatesDTO;
+    }
   }
-}
