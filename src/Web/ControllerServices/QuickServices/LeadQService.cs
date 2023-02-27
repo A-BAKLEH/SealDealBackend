d1e3d3b547d76138bc0ffe0a94518bbf772d8b4e
BasharEskandar <basharo9999@hotmail.com>
2023-02-27 16:29:56 -0500

    lol2

--- src/Web/ControllerServices/QuickServices/LeadQService.cs
+++ src/Web/ControllerServices/QuickServices/LeadQService.cs
@@ -107,7 +107,7 @@ public class LeadQService
     //related Action plan associations should be deleted by cascade
     // Notifs will be deleted automatically. TODO see if u wanna move them to cold storage
     //the outbox handlers u dont have to do anything for now they are enqued on short term failure will be rare
-    await _appDbContext.Database.ExecuteSqlRawAsync($"DELETE FROM [dbo].[ToDoTasks] WHERE LeadId = {leadId} ");
+    await _appDbContext.Database.ExecuteSqlRawAsync($"DELETE FROM [dbo].[ToDoTasks] WHERE LeadId = {leadId};");
     var leadToDelete = new Lead { Id =leadId };
     _appDbContext.Remove(leadToDelete);
     await _appDbContext.SaveChangesAsync();
